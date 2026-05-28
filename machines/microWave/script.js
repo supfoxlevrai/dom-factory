dbb4467c357ad890e8193e08fe31f467d3c712ae
supfoxlevrai <supfoxlevrai@gmail.com>
2026-05-28 21:36:27 +0200

    Ajout des audios du micro-ondes + <audio>

--- machines/microWave/script.js
+++ machines/microWave/script.js
@@ -1,7 +1,16 @@
 //Pour mettre un fichier dans le micro onde
- $('#add').on('click', ()=>{
+let  file_plat1 = ""; ;
+$('#add').on('click', ()=>{
+    if($('#porte').hasClass("fermer")){return;}
     $('#addi').click();
-    $('#audio-a').get(0).play();
+    $('#audio-a').trigger('play');
+});
+
+$('#addi').change(()=>{
+    file_plat1 = $('#addi')[0];
+    let file_plat2 = file_plat1.item(0);
+    let filef = file_plat2.name
+    console.log(filef);
 });
 
 //État du micro onde
@@ -11,10 +20,27 @@ $('#porte').on('click', ()=>{
         $('#porte').text('Ouvert');
         $('#porte').removeClass('fermer');
         $('#porte').addClass('ouvert');
+        $('#audio-o').trigger('play');
     }
     else{
         $('#porte').text('Fermer');
         $('#porte').removeClass('ouvert');
         $('#porte').addClass('fermer');
+        $('#audio-f').trigger('play');
+
     }
+});
+
+$('#start').on('click', ()=>{
+    $('#audio-d').trigger('play');
+
+});
+
+$('#stop').on('click', ()=>{
+    $('#audio-d').trigger('pause');
+    
+});
+
+$('#reset').on('click', ()=>{
+    $('#audio-d').trigger('pause').prop('currentTime', 0)
 });
