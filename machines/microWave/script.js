c9881ce658116020a6d93b2872265e38abd6b58c
supfoxlevrai <supfoxlevrai@gmail.com>
2026-05-28 19:04:08 +0200

    Ajout de fonctionnalité à l'index.html du micro-ondes

---
+++ machines/microWave/script.js
@@ -0,0 +1,6 @@
+//Pour mettre un fichier dans le micro onde
+const buttonAdd = document.getElementById('add');
+const inputAdd = document.getElementById('addi');
+buttonAdd.addEventListener('click', () => {
+    inputAdd.click();
+});
